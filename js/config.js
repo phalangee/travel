453cf3fb99e2c6b0ecf483f86e0d52586b8d1bcb
liuxiaoyan08 <liuxiaoyan08@meituan.com>
2026-09-02 18:20:19 +0800

    更新高德 Key 为 Web 服务 Key，支持静态地图

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
-/* 高德开放平台 Web 端(JS API) Key + 安全密钥。
+/* 高德开放平台 Key 配置
+ * amapKey: 静态地图 API 使用 Web 服务 Key
  * 申请地址: https://console.amap.com/dev/key/app
  * 留空时网站自动进入"降级模式"：地图位置显示有序地点条，仍可逐点跳转高德。 */
 const CONFIG = {
-  amapKey: 'ab10bfdcc0e06478735ae25129b46df8',
-  securityJsCode: 'ae86d3a5bb46afdf05ead56ca5e66293'
+  amapKey: '3b568ec569958f5751a43f1689e20b2b'
 };
